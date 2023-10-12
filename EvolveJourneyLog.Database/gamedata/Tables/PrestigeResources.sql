fb87e790aeacf496ef33e82c9b4623c59050f077
Asbjørn Brandt <abr@norlysenergytrading.com>
2023-10-12 14:26:05 +0200

    Add PrestigeResources table

---
+++ EvolveJourneyLog.Database/gamedata/Tables/PrestigeResources.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE gamedata.PrestigeResources
+(
+    SaveId INT NOT NULL,
+    AICore INT NOT NULL,
+    AntiPlasmid INT NOT NULL,
+    Artifact INT NOT NULL,
+    BloodStone INT NOT NULL,
+    DarkEnergy INT NOT NULL,
+    HarmonyCrystal INT NOT NULL,
+    Phage INT NOT NULL,
+    Plasmid INT NOT NULL,
+
+    CONSTRAINT FK_PrestigeResources_SaveId FOREIGN KEY (SaveId) REFERENCES gamedata.GameSave(SaveId)
+);
+GO
